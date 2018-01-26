a9263ad1aabcc2515e9e5e7eb869eceab8dada74
Casey Forbes <casey@nerdle.com>
2018-01-26 08:37:06 -0500

    Ruby code to split webcomicname title out of the panels

---
+++ ohno_title_splitter.rb
@@ -0,0 +1,104 @@
+require 'chunky_png'
+require 'optparse'
+
+class OhnoTitleSplitter
+
+  def initialize(png_file, percent_black_threshold, neighboring_row_threshold)
+    @canvas = ChunkyPNG::Canvas.from_file(png_file)
+    @image = @canvas.to_image
+    @percent_black_threshold = percent_black_threshold
+    @neighboring_row_threshold = neighboring_row_threshold
+  end
+
+  def save_without_title(output_filename, offset_adjustment)
+    puts "Cropping title at #{title_offset}px and writing to #{output_filename}..."
+
+    offset = title_offset - offset_adjustment
+    cropped = @canvas.crop(0, offset, @image.width, @image.height - offset)
+    cropped.save(output_filename)
+  end
+
+  def save_title_only(output_filename, offset_adjustment)
+    puts "Writing title from 0px-#{title_offset}px to #{output_filename}..."
+
+    offset = title_offset - offset_adjustment
+    cropped = @canvas.crop(0, 0, @image.width, offset)
+    cropped.save(output_filename)
+  end
+
+  def title_offset
+    @title_offset = find_title_offset
+  end
+
+  def is_close_to_black?(r, g, b)
+    r < 40 && g < 40 && b < 40
+  end
+
+  def find_title_offset
+    horizontal_lines = []
+    percent_black_by_row = []
+
+    width = @image.dimension.width
+    height = @image.dimension.height
+
+    (0..(height - 1)).each do |y|
+      black_pixels = (0..(width - 1)).select do |x|
+        pixel = @image[x,y]
+
+        r = ChunkyPNG::Color.r(pixel)
+        g = ChunkyPNG::Color.g(pixel)
+        b = ChunkyPNG::Color.b(pixel)
+
+        is_close_to_black?(r, g, b)
+      end
+
+      percent_black_by_row << black_pixels.length/width.to_f
+    end
+
+    percent_black_by_row.each_with_index do |val, index|
+      p = percent_black_by_row
+      next_3_rows = (0..@neighboring_row_threshold).map {|row| p[index + row] || 0 }
+      if next_3_rows.all? {|black| black > @percent_black_threshold }
+        horizontal_lines << index
+      end
+    end
+
+    horizontal_lines.first || 0
+  end
+
+
+end
+
+
+if __FILE__ == $0
+  input_file = nil
+  output_file = nil
+  output_title = nil
+
+  offset_adjustment = 3
+  percent_black_threshold = 0.40
+  neighboring_row_threshold = 3
+
+  parser = OptionParser.new do |opts|
+    opts.banner = "Usage: ohno_title_splitter.rb [options]"
+
+    opts.on("--input-file input_file", "PNG input file") {|f| input_file = f  }
+    opts.on("--output-title title_file", "Output file name for comic title") {|f| output_title = f }
+    opts.on("--output-file comic_file", "Output file name for comic with title removed")  {|f| output_file = f }
+  end
+  parser.parse!
+
+  unless input_file
+    puts parser.help
+    exit -1
+  end
+
+  splitter = OhnoTitleSplitter.new(input_file, percent_black_threshold, neighboring_row_threshold)
+  if output_title
+    splitter.save_without_title(output_file, offset_adjustment)
+  end
+  if output_file
+    splitter.save_title_only(output_title, offset_adjustment)
+  end
+end
+
